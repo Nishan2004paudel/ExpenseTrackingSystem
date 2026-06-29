4edc39afb9f06db6f3246d681f6a07a6131fc0f7
Nishan2004paudel <neezpau04@gmail.com>
2026-06-29 16:27:36 +0545

    feat: initialization of project till models

---
+++ expensetrackerserver/expensetrackerserver/Models/BudgetLimit.cs
@@ -0,0 +1,14 @@
+﻿namespace expensetrackerserver.Models
+{
+    public class BudgetLimit
+    {
+        public int BudgetId { get; set; }
+        public int UserId { get; set; }
+        public int? CategoryId { get; set; }
+        public decimal BudgetAmount { get; set; }
+        public DateOnly BudgetMonth { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+
+    }
+}
---
+++ expensetrackerserver/expensetrackerserver/Models/User.cs
@@ -0,0 +1,22 @@
+﻿namespace expensetrackerserver.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+
+        public string FullName { get; set; } = string.Empty;
+
+        public string? Profession { get; set; }
+
+        public string PreferredCalendar { get; set; } = string.Empty;
+
+        public bool IsActive { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
