27ad03d3aecb6748ab34b6d95a7a423650b66142
Nishan2004paudel <neezpau04@gmail.com>
2026-07-02 12:02:52 +0545

    feat:jwt authentication

---
+++ expensetrackerserver/expensetrackerserver/Repositories/ICategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using expensetrackerserver.DTOs;
+using expensetrackerserver.Models;
+
+namespace expensetrackerserver.Repositories
+{
+    public interface ICategoryRepository
+    {
+        Task<int> Create(Category category);
+        Task<IEnumerable<Category>> GetAllByUserId(int userId);
+        Task<Category?> GetById(int categoryId);
+        Task<bool> CategoryExists(int userId, string categoryName);
+        Task Update(Category category);
+        Task SoftDelete(int categoryId);
+    }
+}
